f49d34ba611419680bae0f3b0ecc15d9c20d3aea
birdi <egor.birdi7@gmail.com>
2019-07-13 17:18:42 +0300

    Update for aiogram. Closes #1

--- inline_calendar.py
+++ inline_calendar.py
@@ -2,9 +2,14 @@ import datetime
 import logging
 import shelve
 from calendar import monthrange
+from enum import Enum, auto
+from typing import List, Dict
 
 from aiogram import types
+from aiogram.utils.callback_data import CallbackData, CallbackDataFilter
 
+
+# todo move from shelve to json
 
 class WrongCallbackException(Exception):
     pass
@@ -18,6 +23,13 @@ class NotInitedException(Exception):
     pass
 
 
+class Actions(Enum):
+    WRONG_CHOICE = auto()
+    PREVIOUS_MONTH = auto()
+    NEXT_MONTH = auto()
+    PICK_DAY = auto()
+
+
 # constants
 _INLINE_CALENDAR_NAME = 'inline_calendar'
 _MIN_DATE = '_MIN_DATE'
@@ -25,55 +37,51 @@ _MAX_DATE = '_MAX_DATE'
 _CURRENT_DATE = '_CURRENT_DATE'
 _MONTH_NAMES = '_MONTH_NAMES'
 _DAYS_NAMES = '_DAYS_NAMES'
-CALLBACK_WRONG_CHOICE = '{0}_wrong_choice'.format(_INLINE_CALENDAR_NAME)
-CALLBACK_PREVIOUS_MONTH = '{0}_previous_month'.format(_INLINE_CALENDAR_NAME)
-CALLBACK_NEXT_MONTH = '{0}_next_month'.format(_INLINE_CALENDAR_NAME)
-CALLBACK_DAYS = ['{}_day_{}'.format(_INLINE_CALENDAR_NAME, i) for i in range(32)]
+_CALLBACK_DATA_PREFIX = 'inline_calendar'
+CALLBACK = CallbackData(_CALLBACK_DATA_PREFIX, 'action', 'data')
+CALLBACK_WRONG_CHOICE = CALLBACK.new(action=Actions.WRONG_CHOICE.name, data='-')
+CALLBACK_PREVIOUS_MONTH = CALLBACK.new(action=Actions.PREVIOUS_MONTH.name, data='-')
+CALLBACK_NEXT_MONTH = CALLBACK.new(action=Actions.NEXT_MONTH.name, data='-')
+MAX_DAYS_IN_MONTH = 32
+CALLBACK_DAYS = [CALLBACK.new(action=Actions.PICK_DAY.name, data=i) for i in range(MAX_DAYS_IN_MONTH)]
 
 # user vars
 _SHELVE_DB_NAME = "INLINE_CALENDAR_DB"
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def _db_read(chat_id, attr_name):
-    chat_id = str(chat_id)
-    attr_name = str(attr_name)
+
+def _db_read(chat_id: str, attr_name: str):
     try:
         with shelve.open(_SHELVE_DB_NAME) as db:
-            t = db[chat_id]
-            return t[attr_name]
+            return db[chat_id][attr_name]
     except KeyError:
         logging.log(level=logging.CRITICAL, msg='KeyError was raised while getting attribute {} for {}'
-                                                .format(attr_name, chat_id))
+                    .format(attr_name, chat_id))
         # idk why it happens sometimes. Didn't see any affection on work of calendar
 
 
-def _db_write(chat_id, attr_name, data):
-    chat_id = str(chat_id)
-    attr_name = str(attr_name)
-    with shelve.open(_SHELVE_DB_NAME) as db:
-        if chat_id not in db.keys():
-            # first time creation of dictionary
-            db[chat_id] = {}
-        t = db[chat_id]
-        t[attr_name] = data
-        db[chat_id] = t
+def _db_write(chat_id: str, attr_name: str, data):
+    with shelve.open(_SHELVE_DB_NAME, writeback=True) as db:
+        db.setdefault(chat_id, {})[attr_name] = data
+    logger.debug(f'_db_write({chat_id}, {attr_name}. {data})')
 
 
-def _init_db(chat_id):
-    chat_id = str(chat_id)
-    with shelve.open(_SHELVE_DB_NAME, ) as db:
-        db[chat_id] = {}
+def _db_delete(chat_id: str):
+    with shelve.open(_SHELVE_DB_NAME, writeback=True) as db:
+        del db[chat_id]
 
 
-def _create_header(chat_id):
-    result = []
+def _create_header(chat_id: str) -> List[types.InlineKeyboardButton]:
     c_date = _db_read(chat_id, _CURRENT_DATE)
-    result.append(types.InlineKeyboardButton(_db_read(chat_id, _MONTH_NAMES)[c_date.month] + ' ' + str(c_date.year),
-                                             callback_data=CALLBACK_WRONG_CHOICE))
-    return result
+    user_months = _db_read(chat_id, _MONTH_NAMES)
+    return [types.InlineKeyboardButton("{month} {year}".format(month=user_months[c_date.month],
+                                                               year=c_date.year),
+                                       callback_data=CALLBACK_WRONG_CHOICE)]
 
 
-def _create_bottom(chat_id):
+def _create_bottom(chat_id: str) -> List[types.InlineKeyboardButton]:
     result = []
 
     c_date = _db_read(chat_id, _CURRENT_DATE)
@@ -92,19 +100,9 @@ def _create_bottom(chat_id):
     return result
 
 
-def _create_weekdays_buttons(chat_id):
+def _create_weekdays_buttons(chat_id: str) -> List[types.InlineKeyboardButton]:
     d_names = _db_read(chat_id, _DAYS_NAMES)
     return [types.InlineKeyboardButton(d_names[i], callback_data=CALLBACK_WRONG_CHOICE) for i in range(0, 7)]
-
-
-def _check_callback(callback):
-    if callback == CALLBACK_WRONG_CHOICE:
-        return True
-    if callback == CALLBACK_NEXT_MONTH or callback == CALLBACK_PREVIOUS_MONTH:
-        return True
-    if callback in CALLBACK_DAYS:
-        return True
-    return False
 
 
 def _inc_month(chat_id):
@@ -120,9 +118,12 @@ def _dec_month(chat_id):
     _db_write(chat_id, _CURRENT_DATE, prev_month_lastday.replace(day=1))
 
 
-def init(chat_id, base_date, min_date, max_date,
-         month_names=None,
-         days_names=None):
+def init(chat_id,
+         base_date: datetime.date,
+         min_date: datetime.date,
+         max_date: datetime.date,
+         month_names: List[str] = None,
+         days_names: List[str] = None):
     """
     Default language is English
     :param chat_id: chat id
@@ -133,67 +134,59 @@ def init(chat_id, base_date, min_date, max_date,
     :param days_names: 7-element list fo2r month names. If none, then English names will be used
     """
 
-    if not (min_date <= base_date<= max_date):
+    if not (min_date <= base_date <= max_date):
         raise ValueError("Base_date is less than min_date or more than max_date")
 
-    _init_db(chat_id)
-    _db_write(chat_id, _CURRENT_DATE, datetime.date(year=base_date.year, month=base_date.month, day=base_date.day))
-    _db_write(chat_id, _MIN_DATE, datetime.date(year=min_date.year, month=min_date.month, day=min_date.day))
-    _db_write(chat_id, _MAX_DATE, datetime.date(year=max_date.year, month=max_date.month, day=max_date.day))
+    chat_id = str(chat_id)
 
-    m_names = ['-']
-    if not month_names:
-        m_names.extend(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'])
-    else:
-        m_names.extend(month_names)
+    _db_write(chat_id, _CURRENT_DATE, base_date)
+    _db_write(chat_id, _MIN_DATE, min_date)
+    _db_write(chat_id, _MAX_DATE, max_date)
 
-    if len(m_names) != 12 + 1:
-        raise Exception('Length of month names is not 12')
+    if month_names is None:
+        month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
 
-    _db_write(chat_id, _MONTH_NAMES, m_names)
+    if len(month_names) != 12:
+        raise ValueError('Length of month_names != 12')
 
-    if not days_names:
+    _db_write(chat_id, _MONTH_NAMES, month_names)
+
+    if days_names is None:
         days_names = ['Mon', 'Tu', 'Wed', 'Th', 'Fr', 'Sat', 'Sun']
-
     if len(days_names) != 7:
-        raise Exception('Length of days names is not 7')
+        raise Exception('Length of days_names != 7')
 
     _db_write(chat_id, _DAYS_NAMES, days_names)
 
 
-def reset(chat_id):
-    chat_id = str(chat_id)
-    _db_write(chat_id, _CURRENT_DATE, None)
-    _db_write(chat_id, _MIN_DATE, None)
-    _db_write(chat_id, _MAX_DATE, None)
+def reset(chat_id: str):
+    _db_delete(chat_id)
 
 
-def is_inited(chat_id):
-    chat_id = str(chat_id)
+def is_inited(chat_id: str):
     with shelve.open(_SHELVE_DB_NAME) as db:
-        if chat_id not in db.keys():
-            return False
-
-    return _db_read(chat_id, _CURRENT_DATE) is not None
+        return chat_id in db
 
 
 def get_keyboard(chat_id):
+    chat_id = str(chat_id)
     if not is_inited(chat_id):
         raise NotInitedException('inline_calendar is not inited properly')
+
     c_date = _db_read(chat_id, _CURRENT_DATE)
     min_date = _db_read(chat_id, _MIN_DATE)
 
     kb = types.InlineKeyboardMarkup()
-    kb.row(*_create_header(chat_id))
-    kb.row(*_create_weekdays_buttons(chat_id))
+    kb.row(*_create_header(chat_id))  # create header with month name and year aka "Aug 2019"
+    kb.row(*_create_weekdays_buttons(chat_id))  # create buttons with week days
 
     f_row = []
     mrange = monthrange(c_date.year, c_date.month)
-    for i in range(mrange[0]):
+    for i in range(mrange[0]):  # adding the days which were passed
         f_row.append(types.InlineKeyboardButton(text=' ', callback_data=CALLBACK_WRONG_CHOICE))
 
     rows = [f_row]
-    for i in range(1, mrange[1] + 1):
+    for i in range(1, mrange[1] + 1):  # adding active days
         curr_date = datetime.date(day=i, month=c_date.month, year=c_date.year)
         if curr_date.weekday() == 0:
             rows.append([])
@@ -204,45 +197,42 @@ def get_keyboard(chat_id):
 
     curr_date = datetime.date(day=mrange[1], month=c_date.month, year=c_date.year)
 
-    for i in range(curr_date.weekday() + 1, 7):
+    for i in range(curr_date.weekday() + 1, 7):  # adding inactive days
         rows[-1].append(types.InlineKeyboardButton(text=' ', callback_data=CALLBACK_WRONG_CHOICE))
 
     for r in rows:
         kb.row(*r)
 
-    kb.row(*_create_bottom(chat_id))
+    kb.row(*_create_bottom(chat_id))  # adding buttons for pagination
     return kb
 
 
-def is_inline_calendar_callbackquery(query):
-    return _check_callback(query.data)
+def filter(**config) -> CallbackDataFilter:
+    return CALLBACK.filter(**config)
 
 
-def handler_callback(chat_id, callback):
+def handle_callback(chat_id, callback_data: Dict[str, str]):
     """
     A method for handling callbacks
     :param chat_id: chat id of user
-    :param callback: callback from telebot.types.CallbackQuery
+    :param callback_data:
     :return: datetime.date object if some date was picked else None
     """
+    chat_id = str(chat_id)
+
     if not is_inited(chat_id):
         raise NotInitedException('inline_calendar is not inited properly')
-
-    if not _check_callback(callback):
-        raise WrongCallbackException('Wrong callback is given for handling')
-
-    if callback == CALLBACK_WRONG_CHOICE:
-        return None
 
     c_date = _db_read(chat_id, _CURRENT_DATE)
     min_date = _db_read(chat_id, _MIN_DATE)
     max_date = _db_read(chat_id, _MAX_DATE)
-    if callback == CALLBACK_PREVIOUS_MONTH and c_date > min_date:
+    logger.debug(f"callback_data={callback_data}")
+    if callback_data['action'] == Actions.PREVIOUS_MONTH.name and c_date > min_date:
         _dec_month(chat_id)
-        return None
-    if callback == CALLBACK_NEXT_MONTH and c_date < max_date:
-        _inc_month(chat_id)
-        return None
 
-    if callback in CALLBACK_DAYS:
-        return _db_read(chat_id, _CURRENT_DATE).replace(day=int(callback.split('_')[-1]))
+    if callback_data['action'] == Actions.NEXT_MONTH.name and c_date < max_date:
+        _inc_month(chat_id)
+
+    if callback_data['action'] == Actions.PICK_DAY.name:
+        logger.debug('pick day!')
+        return _db_read(chat_id, _CURRENT_DATE).replace(day=int(callback_data['data']))
