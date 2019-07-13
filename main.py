6e4c46a2a3b7783601ca1cf1286897e17a6297b7
birdi <egor.birdi7@gmail.com>
2019-07-13 17:19:12 +0300

    Uodate example for a new version of calendar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import types, Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.exceptions import MessageNotModified
 
 import inline_calendar
 
@@ -24,11 +25,11 @@ async def calendar_test(msg: types.Message):
     await bot.send_message(msg.from_user.id, text='test', reply_markup=inline_calendar.get_keyboard(msg.from_user.id))
 
 
-@dp.callback_query_handler(fun=inline_calendar.is_inline_calendar_callbackquery)
-async def calendar_callback_handler(q: types.CallbackQuery):
+@dp.callback_query_handler(inline_calendar.filter())
+async def calendar_callback_handler(q: types.CallbackQuery, callback_data: dict):
     await bot.answer_callback_query(q.id)
 
-    return_data = inline_calendar.handler_callback(q.from_user.id, q.data)
+    return_data = inline_calendar.handle_callback(q.from_user.id, callback_data)
     if return_data is None:
         await bot.edit_message_reply_markup(chat_id=q.from_user.id, message_id=q.message.message_id,
                                             reply_markup=inline_calendar.get_keyboard(q.from_user.id))
@@ -37,6 +38,10 @@ async def calendar_callback_handler(q: types.CallbackQuery):
         await bot.edit_message_text(text=picked_data, chat_id=q.from_user.id, message_id=q.message.message_id,
                                     reply_markup=inline_calendar.get_keyboard(q.from_user.id))
 
+
+@dp.errors_handler(exception=MessageNotModified)
+async def error_handler(update, error):
+    return True
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
