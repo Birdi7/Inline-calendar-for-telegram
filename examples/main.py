971f18ed34bfea5aaad1da9d3117300c5c0b1b19
birdi <egor.birdi7@gmail.com>
2019-07-14 17:00:47 +0300

    Update example for the new version

--- examples/main.py
+++ examples/main.py
@@ -5,7 +5,7 @@ from aiogram import types, Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.exceptions import MessageNotModified
 
-import inline_calendar as inline_calendar
+from inline_calendar.inline_calendar import InlineCalendar
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -14,15 +14,15 @@ YOUR_TOKEN = 'TOKEN'
 
 bot = Bot(token=YOUR_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+inline_calendar = InlineCalendar()
 
 
 @dp.message_handler(commands=['calendar'])
 async def calendar_test(msg: types.Message):
-    inline_calendar.init(msg.from_user.id,
-                         datetime.date.today(),
+    inline_calendar.init(datetime.date.today(),
                          datetime.date.today() - datetime.timedelta(weeks=16),
                          datetime.date.today() + datetime.timedelta(weeks=16))
-    await bot.send_message(msg.from_user.id, text='test', reply_markup=inline_calendar.get_keyboard(msg.from_user.id))
+    await bot.send_message(msg.from_user.id, text='test', reply_markup=inline_calendar.get_keyboard())
 
 
 @dp.callback_query_handler(inline_calendar.filter())
@@ -36,7 +36,7 @@ async def calendar_callback_handler(q: types.CallbackQuery, callback_data: dict)
     else:
         picked_data = return_data
         await bot.edit_message_text(text=picked_data, chat_id=q.from_user.id, message_id=q.message.message_id,
-                                    reply_markup=inline_calendar.get_keyboard(q.from_user.id))
+                                    reply_markup=inline_calendar.get_keyboard())
 
 
 @dp.errors_handler(exception=MessageNotModified)
